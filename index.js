1fc06186408caa77900cc229003ff1533ccfcb9b
Raihan Preena <raihanpreena@gmail.com>
2021-02-27 21:49:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+  return string.toUpperCase ()
+}
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logShout(string) {
+console.log (string.toUpperCase ())
+}
+function logWhisper(string) {
+console.log (string.toLowerCase ())
+}
+function sayHiToGrandma (string) {
+  var cant = "I can't hear you!";
+  var yes = "yes, indeed!";
+  var love = "I love you, too!";
+  if (string.tolowercase sayHiToGrandma === sayHiToGrandma) {
+    return cant;
+   else if (string.toUpperCasecase sayHiToGrandma === sayHiToGrandma) {
+    return yes;
+  else if ("I love you Grandma." === sayHiToGrandma) {
+  return love
+  }
+}
+}
+}
