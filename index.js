5016249a47de76ddb72ac45cde070dbd22fc29e0
Raihan Preena <raihanpreena@gmail.com>
2021-02-27 21:54:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ function logWhisper(string) {
 console.log (string.toLowerCase ())
 }
 function sayHiToGrandma (string) {
-  var cant = "I can't hear you!";
-  var yes = "yes, indeed!";
-  var love = "I love you, too!";
-  if (string.tolowercase sayHiToGrandma === sayHiToGrandma) {
+  var cant = "I can't hear you!"
+  var yes = "yes, indeed!"
+  var love = "I love you, too!"
+  if (string.tolowercase sayHiToGrandma === sayhitograndma) {
     return cant;
-   else if (string.toUpperCase sayHiToGrandma === sayHiToGrandma) {
+   else if (string.toUpperCase sayHiToGrandma === SAYHITOGRANDMA) {
     return yes;
   else if ("I love you Grandma." === sayHiToGrandma) {
   return love
