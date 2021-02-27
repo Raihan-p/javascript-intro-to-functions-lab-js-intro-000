3d41833ff7dc40ec9aa91fdf08725fba99e4aeff
Raihan Preena <raihanpreena@gmail.com>
2021-02-27 22:27:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,11 @@ function sayHiToGrandma (string) {
   var love = "I love you, too!";
   if (string.tolowercase (string) === string) {
     return cant;
-  }
+}
   else if (string.toUpperCase (string) === string) {
     return yes;
-  }
+}
   else if ("I love you Grandma." === string) {
   return love
-  }
+}
+}
