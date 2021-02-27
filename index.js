afec0c60e149333d0650426d3fff39e095dcbc92
Raihan Preena <raihanpreena@gmail.com>
2021-02-27 22:40:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ console.log (string.toLowerCase ())
 function sayHiToGrandma (string) {
   var cant = "I can't hear you!";
   var yes = "YES INDEED!";
-  var love = "I love you, too!";
+  var love = "I love you, too";
   if (string.toLowerCase (string) === string) {
     return cant;
 }
